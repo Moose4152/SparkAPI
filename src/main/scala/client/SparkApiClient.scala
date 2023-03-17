f10c69d5b06e0950b9ad9f3abcecaefa1bb2e417
MayankSinghRana <mayanksinghrana@MayankSinghRanas-MacBook-Air.local>
2023-03-17 22:54:39 +0530

    changed thrift branch

--- src/main/scala/client/SparkApiClient.scala
+++ src/main/scala/client/SparkApiClient.scala
@@ -1,23 +1,41 @@
 package client
 import org.apache.thrift.protocol.{TBinaryProtocol, TProtocol}
 import org.apache.thrift.transport.{TSocket, TTransport}
-import com.github.msr.SparkAPI
-import scala.jdk.CollectionConverters.IterableHasAsScala
+import com.github.msr.{FileType, ReadData, SQLStatment, SparkAPI}
 
+import scala.jdk.CollectionConverters.IterableHasAsScala
 object SparkApiClient  extends App{
   val port = 9103
   val host = "127.0.0.1"
 
-  def execute_query():Unit = {
-    val query = "select hour,k1,m1 from RawTable limit 10"
+  def readData():Unit = {
+
     val transport = new TSocket(host,port)
     val protocol = new TBinaryProtocol(transport)
     val client = new SparkAPI.Client(protocol)
     transport.open()
-    val res = client.exectureCommand(query)
+    val Query = new ReadData()
+    Query.setPath("/Users/mayanksinghrana/Desktop/TdigestExp/Functional/rollup1/")
+    Query.setDataType(FileType.Parquet)
+    val res = client.readFile(Query)
     println(res)
   }
 
-  execute_query()
+
+
+  def execute_statment() = {
+    val query = "select hour,m1 from RAW limit 10"
+    val transport = new TSocket(host,port)
+    val protocol = new TBinaryProtocol(transport)
+    val client = new SparkAPI.Client(protocol)
+    transport.open()
+    val SQLStatment = new SQLStatment()
+    SQLStatment.setSql_statment(query)
+    val res = client.exectureCommand(SQLStatment)
+    println(res)
+  }
+
+  readData()
+  execute_statment()
 
 }
